0d56d398213ed57b9dd4781752f17eabaf56f99d
lopessoftec <lopessoftec@gmail.com>
2021-10-21 22:15:47 -0300

    desenvolvendo formulario

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,13 +20,13 @@ export default function App() {
     Poppins_700Bold
   });
 
-  if(!fontsLoaded){
+  if (!fontsLoaded) {
     return <AppLoading />
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+
+import { Button } from '../../components/Form/Button';
+
+import { categories } from '../Utils/categories';
+import {
+    Container,
+    Header,
+    Title,
+    Category,
+    Icon,
+    Name,
+    Separator,
+    Footer
+} from './styles';
+
+interface Category {
+    key: string;
+    name: string;
+}
+
+interface Props {
+    category: Category;
+    setCategory: (Category: Category) => void;
+    closeSelectCategory: () => void;
+}
+
+export function CategorySelect({
+    category,
+    setCategory,
+    closeSelectCategory
+}: Props) {
+    function handleCategorySelect(category: Category) {
+        setCategory(category);
+    }
+
+    return (
+        <Container>
+            <Header>
+                <Title>Categoria</Title>
+            </Header>
+
+            <FlatList
+                data={categories}
+                style={{ flex: 1, width: '100%' }}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => (
+                    <Category
+                        onPress={() => handleCategorySelect(item)}
+                        isActive={category.key === item.key}
+                    >
+                        <Icon name={item.icon} />
+                        <Name>{item.name}</Name>
+                    </Category>
+                )}
+                ItemSeparatorComponent={() => <Separator />}
+            />
+
+            <Footer>
+                <Button
+                    title="Selecionar"
+                    onPress={closeSelectCategory}
+                />
+            </Footer>
+        </Container>
+    )
+}
