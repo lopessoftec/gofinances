6dda4ea5ea7942b351123da2c0835e4ae2f87b7c
lopessoftec <lopessoftec@gmail.com>
2021-12-01 21:53:07 -0300

    listando transacoes

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { categories } from '../../screens/Utils/categories';
 
 import {
     Container,
@@ -11,16 +12,11 @@ import {
     Date
 } from './styles';
 
-interface Category {
-    name: string;
-    icon: string;
-}
-
 export interface TransactionCardProps {
     type: 'positive' | 'negative';
-    title: string;
+    name: string;
     amount: string;
-    category: Category;
+    category: String;
     date: string;
 
 }
@@ -30,10 +26,14 @@ interface Props {
 }
 
 export function TransactionCard({ data }: Props) {
+    // pego a primeira posição e nomeio de category
+    const [ category ] = categories.filter(
+        item => item.key === data.category
+    );
     return (
         <Container>
             <Title>
-                {data.title}
+                {data.name}
             </Title>
 
             <Amount type={data.type}>
@@ -43,9 +43,9 @@ export function TransactionCard({ data }: Props) {
 
             <Footer>
                 <Category>
-                    <Icon name={data.category.icon} />
+                    <Icon name={category.icon} />
                     <CategoryName>
-                        {data.category.name}
+                        {category.name}
                     </CategoryName>
                 </Category>
 
