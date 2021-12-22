a2f83711018d7b4a8067efaf0e1e5e6e37584872
lopessoftec <lopessoftec@gmail.com>
2021-12-21 22:58:29 -0300

    criando contexto

--- App.tsx
+++ App.tsx
@@ -21,6 +21,8 @@ import { AppRoutes } from './src/routes/app.routes';
 
 import { SignIn } from './src/screens/SignIn';
 
+import { AuthContext } from './src/AuthContext';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -36,7 +38,11 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <SignIn />
+
+        <AuthContext.Provider value={['Ytalo']}>
+          <SignIn />
+        </AuthContext.Provider>
+
       </NavigationContainer>
     </ThemeProvider>
   )
