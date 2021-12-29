68b22ba13a67302990ad06b81f21b9d338c3924b
lopessoftec <lopessoftec@gmail.com>
2021-12-28 22:59:26 -0300

    vinculando as transacoes ao usuario

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -16,7 +16,7 @@ export const Container = styled(GestureHandlerRootView)`
 
 export const Header = styled.View`
     width: 100%;
-    height: ${RFValue(113)};
+    height: ${RFValue(113)}px;
 
     background-color: ${({ theme }) => theme.colors.primary};
     
