90f80323b605fb62fb6bf231b61468916ecc81ed
lopessoftec <lopessoftec@gmail.com>
2022-05-30 23:02:39 -0300

    Usando seletores

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -9,3 +9,21 @@ test('check if show correctly user input name placeholder', () => {
     const inputName = getByPlaceholderText('Nome');
     expect(inputName).toBeTruthy();
 });
+
+test('checks if user data has been loaded', () => {
+    const { getByTestId } = render(<Profile />);
+
+    const inputName = getByTestId('input-name');
+    const inputSurname = getByTestId('input-surname');
+
+    expect(inputName.props.value).toEqual('Ytalo');
+    expect(inputSurname.props.value).toEqual('Lopes');
+});
+
+test('checks if title render correctly', () => {
+    const { getByTestId } = render(<Profile />);
+
+    const textTitle = getByTestId('text-title');
+
+    expect(textTitle.props.children).toContain('Perfil');
+})
